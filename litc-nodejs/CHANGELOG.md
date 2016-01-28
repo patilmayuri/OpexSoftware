3f58477a0ed5113ecee1bf8fb73b781977d71359
mayuri patil <mayuri.patil@opexsoftware.com>
2016-01-28 06:10:23 -0800

    file deleted

--- litc-nodejs/CHANGELOG.md
+++
@@ -1,6 +0,0 @@
-litc-nodejs CHANGELOG
-==================
-
-0.1.0
------
-- [nitin_chandurkar] - Initial release of litc-nodejs
--- litc-nodejs/metadata.rb
+++
@@ -1,9 +0,0 @@
-name             'litc-nodejs'
-maintainer       'Opex Software'
-maintainer_email 'nitin.chandurkar@opexsoftware.com'
-license          'All rights reserved'
-description      'Installs Nodejs'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
-supports         'windows'
-depends          'windows'
